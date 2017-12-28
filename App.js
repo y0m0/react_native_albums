83912f24affdd3cb9520220ab84c5876c4a1d569
y0m0 <lixi.daniele@gmail.com>
2017-12-28 19:51:09 +0100

    Enable scrolling by using the ScrollView react-native component add a flex property of 1 to the main view to properly display the last element

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default class App extends Component<{}> {
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <Header headerText={'Albums!'} />
         <AlbumList />
       </View>
