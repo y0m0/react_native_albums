641de01c54c705fc29848948cd2aa3f339c8ed68
y0m0 <lixi.daniele@gmail.com>
2017-12-28 19:20:29 +0100

    Add a view section for the album name and album artist

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
   return (
     <Card>
-      <Text>{props.album.title}</Text>
+      <CardSection>
+        <View></View>
+        <View style={styles.headerContentStyle}>
+          <Text>{props.album.title}</Text>
+          <Text>{props.album.artist}</Text>
+        </View>
+      </CardSection>
     </Card>
   );
 };
 
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  }
+};
 export default AlbumDetail;
