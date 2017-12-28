eb30630f5d58ccc2914909fbfe91014422d4722c
y0m0 <lixi.daniele@gmail.com>
2017-12-28 15:21:57 +0100

    Add style for the header component

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import Header from './src/components/header'
+import Header from './src/components/header';
 
 export default class App extends Component<{}> {
   render() {
