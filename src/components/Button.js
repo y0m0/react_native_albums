844c907f5b66a414ed4480d201c42c3b751552da
y0m0 <lixi.daniele@gmail.com>
2017-12-29 19:21:36 +0100

    Dinamically change the button text

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
 
   return (
@@ -10,7 +10,7 @@ const Button = ({ onPress }) => {
       style={buttonStyle}
     >
       <Text style={textStyle}>
-        Click me!
+        {children}
       </Text>
     </TouchableOpacity>
   );
